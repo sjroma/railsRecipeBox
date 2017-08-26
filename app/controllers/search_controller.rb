10842cc4983cce9ab9ad24296cec23e25f524d42
Steve Roma <sjroma@hotmail.com>
2017-08-26 11:24:15 -0400

    start to add search capability

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,8 @@
+class SearchController < ApplicationController
+	def search
+		if params[:search]
+			@recipe = Recipe.find(:all, :conditions => ['name LIKE ?', "%#{params[:search]}%"])
+		else
+			@recipe = Recipe.find(:all)
+	end
+end
